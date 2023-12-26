ac1e7289248ce4a4a36cc4e942a231827df423ab
Akhil Baidya <akhilbaidya101@gmail.com>
2023-12-26 12:16:48 -0800

    drafted code for deleting function (takes out the wanted node from the linked list and then deletes it)

--- studentList.cpp
+++ studentList.cpp
@@ -26,7 +26,7 @@ using namespace std;
 
 void ADD(Node* &prevNode, Node* &newNode);   
 void PRINT(Node* &header);
-void DELETE(Node* &header);
+void DELETE(Node* &header, int id);
 void QUIT(Node* &header, bool &running);
 void AVERAGE(Node* &header);
 
@@ -129,87 +129,50 @@ void ADD(Node* &prevNode, Node* &newNode) {
 /* The PRINT() function takes in the current vector of students (student pointers) and
 prints out each student registered (and their info).
 */
-void PRINT(Node* &stud1) {
+void PRINT(Node* &header) {
 
-  if (stud1 -> getNext() == NULL) {
-    return;
+  if (header -> getNext() == NULL) {
+    return; //there are no students to print
   }
 
-  Student* student = stud1 -> getStudent();
+  Student* student = header -> getStudent(); //get the student
   cout << student -> getFirstN() << " ";
   cout << student -> getSecondN() << ", ";
   cout << "ID: " << student -> getId() << ", ";
   cout << "GPA: " << student -> getGpa() << endl;
 
-  PRINT(stud1 -> getNext()); //recurse and print out more!
+  PRINT(header -> getNext()); //recurse and print out more!
   return;
-    
-    /*
-  for (vector<Student*>::iterator student = studVec.begin(); student != studVec.end(); student++) { //iterate through each student in the vector
-
-    referred to Mr. Galbraith's video on Vectors in Canvas to learn about using "->" to access aspects of an object pointed at
-     (link in heading comments)
-
-    cout << (*student) -> firstName << " "; //print out the first and last name of the student
-    cout << (*student) -> secondName << ", ";
-
-    cout << "ID: " << (*student) -> id << ", "; //print out the student's id
-
-    referred to Mr. Galbraith's video on Formatting Outputs in Canvas to learn about .setf() and .precision()
-      (link in heading comments)
-    
-    cout.setf(ios::showpoint); //want trailing zeroes in gpa (5 as 5.00)
-    cout.precision(3); //restrict precision to three, allowing two decimal places for gpa
-    cout << "GPA: " << (*student) -> gpa << endl; //print out the student's gpa
-  }
-  
-  return;
-*/
 }
 
 /* The DELETE() function takes in the current vector of students (student pointers) and
 prompts the user for a student id. It then erases the student with that id from the student list.
 */
-void DELETE(Node* &stud1) {
-  /*
-  //referred to this source for how to delete objects in a vector (with the .erase() command:
-  //https://www.geeksforgeeks.org/cpp-stl-cheat-sheet/#T3
+void DELETE(Node* &header, int id) {
 
-  This source shows how the .erase() command takes the position of an object
-    in a vector and deletes the object at that position.
-
-    The source also shows how the beginning position (of the first element
-    in the vector) can be accessed through vectorName.begin())
-   
+  if (header -> getNext() == NULL) {
+    return;
+  }
   
-  int rmID; //the id of the student that will be removed
-  int count = 0; //counts the distance each "student" is away from the beginning
-  int position = -1; //the position of the student needed to be deleted
- 
-  cout << "Which student do you want to remove from the student list? (Give ID)" << endl;
-  cin >> rmID; //get the id of the student we want to remove
+  Node* theNode = header -> getNext();
+  Student* student = theNode -> getStudent();
 
-
-    for (vector<Student*>::iterator student = studVec.begin(); student != studVec.end(); student++) { //iterate through each student in the vector
-
-    ++count; //increase the count (creates a "number" for each students position from studVec.begin())
-
-     if (((*student) -> id) == rmID) { //if the student's id is equal to the id being searched for..
-      position = count; //this is the position of the student we want removed!
-      delete (*student); //help from Mr. Galbraith for delete command (deletes the Struct pointed to by the student pointer - removes the actual student)
-
-    }
+  if (student -> getId() == id) {
+    Node* nextNode = theNode -> getNext();
+    theNode -> setNext(NULL);
+    header -> setNext(nextNode); //move the deleted nodes next node to the prev nodes next node
+    delete theNode;
+    return;
   }
 
-    if (position != -1) { //if there is a student (position) with that id
-      studVec.erase(studVec.begin() + position - 1); //remove student pointer at that position from the vector
+  DELETE(theNode, id);
+  return;
+}
 
-      cout << "removed student" << endl;
-    }
+bool QUIT(Node* &header, bool running) {
+  return false;
+}
 
-else {
-      cout << "student not found" << endl;
-    }
-  */
+void AVERAGE(Node* &header) {
   return;
 }
