70bacde4ccfb92a3945c3ddf04471a1132eaf9de
Akhil Baidya <akhilbaidya101@gmail.com>
2023-12-26 12:54:08 -0800

    added something to Ghazi Abbas' node class constructor to see if that fixes the Node.o file

--- node.h
+++ node.h
@@ -1,13 +1,13 @@
 #ifndef NODE_H
 #define NODE_H
-#include "student.h"
+#include "Student.h"
 using namespace std;
 
 class Node 
 {
-    public:
+public:
     //constructor
-    Node(Student*);
+  Node(Student* student); //Akhil: editing this to have name of variable
     //destructor
     ~Node();
 
@@ -15,12 +15,11 @@ class Node
     Node* getNext();
     Student* getStudent();
     void setNext(Node* nNext);
-    private: 
+
+private: 
     Student* student;
     Node* next;
 
 };
-
-
 
 #endif
