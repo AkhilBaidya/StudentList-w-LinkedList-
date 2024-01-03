64a7673e153d5f70b303388b5ec6f0c44ae77dbf
Akhil Baidya <akhilbaidya101@gmail.com>
2024-01-02 18:20:08 -0800

    commented

--- Student.cpp
+++ Student.cpp
@@ -1,26 +1,35 @@
+//C++ Programming: Linked List Project - Student Class Definitions
+//by Akhil Baidya
+
+//Date of Submission: 01/03/24
+
+/*Note: Here, the methods of the student class are defined. This includes definitions for the constructor, destructor, and get and set methods (that get and set field values such as id, gpa, first name, and last name).*/
+
 #include <iostream>
 #include <cstring>
 #include "Student.h"
 
 using namespace std;
 
-Student::Student(int theId, float theGpa, char theFirstN[20], char theSecN[20]) { //constructor
-  id = theId;
-  gpa = theGpa;
+//Constructor:
+Student::Student(int theId, float theGpa, char theFirstN[20], char theSecN[20]) {
+  id = theId; //set id
+  gpa = theGpa; //set gpa
 
-  firstName = new char[20];
-  strcpy(firstName, theFirstN);
+  firstName = new char[20]; //create space for first name
+  strcpy(firstName, theFirstN); //set first name
 
-  secondName = new char[20];
-  strcpy(secondName, theSecN);
+  secondName = new char[20]; //create space for last name
+  strcpy(secondName, theSecN); //set last name
 }
 
-Student::~Student() { //destructor
-  delete[] firstName;
+//Destructor:
+Student::~Student() { 
+  delete[] firstName; //destroy the character arrays
   delete[] secondName;
 }
 
-//get methods:
+//Get methods:
 int Student::getId() {
   return id;
 }
@@ -34,7 +43,7 @@ char* Student::getSecondN() {
   return secondName;
 }
 
-//set methods:
+//Set methods:
 void Student::setId(int newId) {
   id = newId;
 }
@@ -42,11 +51,11 @@ void Student::setGpa(float newGpa) {
   gpa = newGpa;
 }
 void Student::setFirstN(char newFirstN[20]){
-  firstName = new char[20];
+  firstName = new char[20]; //first create space for the first name
   strcpy(firstName, newFirstN);
 }
 void Student::setSecondN(char newSecN[20]) {
-  secondName = new char[20];
+  secondName = new char[20]; //first create space for the last name
   strcpy(secondName, newSecN);
 }
 
