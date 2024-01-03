9cb8c857395f26c105bbff52c6df867d866effc4
Akhil Baidya <akhilbaidya101@gmail.com>
2024-01-02 18:19:50 -0800

    commented

--- Student.h
+++ Student.h
@@ -1,3 +1,10 @@
+//C++ Programming: Linked List Project - Student Class Header File
+//by Akhil Baidya
+
+//Date of Submission: 01/03/24
+
+/*Note: Here, the methods and fields of the student class are stated. This includes the constructor, destructor, and get and set methods, as well as fields such as the student id, gpa, and name.*/
+
 #include <iostream>
 #include <cstring>
 
@@ -6,26 +13,27 @@ using namespace std;
 class Student {
 
  public:
-  //constructor:
-  Student(int theId, float theGpa, char theFirstN[20], char theSecN[20]);
 
-  //destructor:
+  //Constructor:
+  Student(int theId, float theGpa, char theFirstN[20], char theSecN[20]);
+  
+  //Destructor:
   ~Student();
 
-  //get methods:
+  //Get methods:
   int getId();
   float getGpa();
   char* getFirstN();
   char* getSecondN();
 
-  //set methods:
+  //Set methods:
   void setId(int newId);
   void setGpa(float newGpa);
   void setFirstN(char newFirstN[20]);
   void setSecondN(char newSecN[20]);
  
  protected:
-  //fields
+  //Fields
   int id;
   float gpa;
   char* firstName;
